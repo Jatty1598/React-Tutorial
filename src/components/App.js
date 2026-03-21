c4c9e1fd130db4f41494fcf1a0bce4316eacb994
Jatty1598 <jatin.allawadi98@gmail.com>
2026-03-21 17:11:38 +0530

    useEffect changes and add and delete contact functionality

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,39 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { v4 as uuidv4 } from 'uuid';
 import './App.css';
 import Header from './Header';
 import AddContact from './AddContact';
 import ContactList from './ContactList';
 
 function App() {
-  const[contacts, setContacts] = useState([]);
+  const LOCAL_STORAGE_KEY = "contacts"
+  const[contacts, setContacts] = useState(() => {
+    const saved = localStorage.getItem(LOCAL_STORAGE_KEY)
+    return saved ? JSON.parse(saved) : []
+  });
+
   const addContactHandler = (contact) => {
     console.log(contact)
-    setContacts([...contacts,{...contact, id: Date.now()}])
+    setContacts([...contacts,{id : uuidv4(),...contact}])
   }
+
+  const removeContactHandler = (id) => {
+    const newContactList =contacts.filter((contact) => {
+      return contact.id !== id;
+    });
+
+    setContacts(newContactList)
+  }
+
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(contacts))
+  },[contacts])
+
   return (
     <div className="ui container">
       <Header />
       <AddContact addContactHandler = {addContactHandler}/>
-      <ContactList contacts={contacts}/>
+      <ContactList contacts={contacts} getContactId={removeContactHandler}/>
     </div>
   );
 }
--- src/components/ContactCard.js
+++ src/components/ContactCard.js
@@ -2,7 +2,7 @@ import React from "react";
 import user from '../images/user.png';
 
 const ContactCard = (props) => {
-    const {name,email} = props.contact
+    const {id,name,email} = props.contact
     return (
         <div className="item">
             <div className="content">
@@ -11,7 +11,9 @@ const ContactCard = (props) => {
                 <div className="header">{email}</div>
             </div>
             <i className="trash alternate outline icon"
-            style={{color:"red",marginTop:"7px"}}></i>
+            style={{color:"red",marginTop:"7px"}}
+            onClick={() => props.clickHandler(id)}
+            ></i>
         </div>
     )
 }
